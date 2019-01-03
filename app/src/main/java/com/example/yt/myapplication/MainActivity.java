531aa15a2ceb9d1cfcf7e49047f33aefed9abb9e
leioalba <294075195@qq.com>
2019-01-03 09:52:03 +0800

    misaka

--- app/src/main/java/com/example/yt/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yt/myapplication/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.yt.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if (CheckNet.getNetState(this)==CheckNet.NET_NONE){
+            Log.d("天气预报","网络不通");
+            Toast.makeText(MainActivity.this,"网络不通",Toast.LENGTH_LONG).show();
+        }else{
+            Log.d("天气预报","网络畅通");
+            Toast.makeText(MainActivity.this,"网络畅通",Toast.LENGTH_LONG).show();
+        }
 
 
     }
